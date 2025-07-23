f8f8f1ccf119a99ecc0677eebd9ffc8f25160e40
rezaali2290 <rezaali20ab@gmail.com>
2025-07-24 01:08:20 +0545

    payment route

--- routes/PaymentRoute.js
+++ routes/PaymentRoute.js
@@ -6,6 +6,8 @@ const {
 const router = express.Router();
 const { isAuthenticatedUser } = require("../middleware/auth");
 
+router.route("/payment/process").post(isAuthenticatedUser, Payment);
 
+router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
 
 module.exports = router;
