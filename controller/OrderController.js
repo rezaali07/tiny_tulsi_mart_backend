11fd017db51b0972b171addb5bdf84dbe07e350c
rezaali2290 <rezaali20ab@gmail.com>
2025-07-24 00:04:56 +0545

    updateOrderAdmin logic

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -65,7 +65,42 @@ exports.getOrderHistory = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json(orderHistory);
 });
 
+//update order status === admin
+exports.updateOrderAdmin = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
 
+  if (!order) {
+    return next(new ErrorHandler("Order did not find with this Id", 404));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("Order is already delivered", 400));
+  }
+
+  if (req.body.status === "Shipped") {
+    order.orderItems.forEach(async (o) => {
+      await updateStock(o.product, o.quantity);
+    });
+  }
+  order.orderStatus = req.body.status;
+
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({ validateBeforeSave: false });
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock -= quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
 // delete order
 exports.deleteOrderAdmin = catchAsyncErrors(async (req, res, next) => {
   const order = await Order.findById(req.params.id);
