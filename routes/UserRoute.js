94308adf7bfccb3b18178f50e3c3e78594b152cc
rezaali2290 <rezaali20ab@gmail.com>
2025-07-23 23:46:39 +0545

    user route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -9,6 +9,14 @@ const {
  
 
 
+router.route("/me/sessions").get(isAuthenticatedUser, getUserSessions);
+router.route("/sessions/:token").delete(isAuthenticatedUser, logoutSessionByToken);
+router.route("/logout-all").post(isAuthenticatedUser, logoutAllSessions);
+router.get("/me/current-token", isAuthenticatedUser, getCurrentToken);
+
+// OTP routes for registration and login
+router.post("/send-otp", sendOtpToEmail);
+router.post("/verify-otp-register", verifyOtpAndCreateUser);
 
 
 module.exports = router;
