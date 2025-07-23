7c51e38596e19c4b33593653a8d16f66faa24968
rezaali2290 <rezaali20ab@gmail.com>
2025-07-24 00:38:56 +0545

    payment using stripe controller

--- controller/PaymentController.js
+++ controller/PaymentController.js
@@ -1,6 +1,5 @@
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
-const stripe = require("stripe")("sk_test_51QOycTII53sZYyCwdnpBPYVyWpG0ABR1ZKFRZW5Ac5aYuUtS18qyJan4Z4PbsatHPpcmBZ5hUppJm4QciBqQL39Z00WecBHpQJ");
 
 exports.Payment = catchAsyncErrors(async (req, res, next) => {
   const myPayment = await stripe.paymentIntents.create({
