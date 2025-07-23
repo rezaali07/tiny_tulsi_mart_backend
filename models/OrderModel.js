df22d2c2eb017a6c6ef0dcb1b964a58815817f18
rezaali2290 <rezaali20ab@gmail.com>
2025-07-24 00:28:23 +0545

    order Model

---
+++ models/OrderModel.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const OrderSchema = new mongoose.Schema({
+  shippingInfo: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      // required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    pinCode: {
+      type: String,
+      // required: true,
+    },
+    phoneNo: {
+      type: Number,
+      required: true,
+    },
+  },
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  paymentInfo: {
+    id: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  
+
+module.exports = mongoose.model("Order", OrderSchema);
