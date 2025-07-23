ea1396e40488c67e3f9b2fc854176e9da1adf299
rezaali2290 <rezaali20ab@gmail.com>
2025-07-24 00:25:50 +0545

    validateBase64Image

--- utils/validateBase64Image.js
+++ utils/validateBase64Image.js
@@ -4,7 +4,9 @@ const validateBase64Image = (base64String) => {
   const matches = base64String.match(/^data:(.+);base64,(.+)$/);
   if (!matches || matches.length !== 3) return false;
 
-  
+  const mimeType = matches[1];
+  const validTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
+  return validTypes.includes(mimeType);
 };
 
 module.exports = validateBase64Image;
