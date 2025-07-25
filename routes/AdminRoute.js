2f76b3bd06c4ebee2f5012331c0823ba446f635f
rezaali2290 <rezaali20ab@gmail.com>
2025-07-26 00:22:36 +0545

    admin remaing routes

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const { getAuditLogs } = require("../controller/AdminController");
 const { isAuthenticatedUser, authorizedRoles } = require("../middleware/auth");
 
-
+// Only admins can access this route
+router.get("/admin/audit-logs", isAuthenticatedUser, authorizedRoles("admin"), getAuditLogs);
 
 module.exports = router;
