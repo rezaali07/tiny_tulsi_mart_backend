1b3bd1eac280b2e4f4a85fc1d1d9110c56bd0870
rezaali2290 <rezaali20ab@gmail.com>
2025-07-26 00:35:29 +0545

    reaming utils logic

---
+++ utils/generateOtp.js
@@ -0,0 +1,15 @@
+// const otp = otpGenerator.generate(6, {
+//   digits: true,
+//   upperCaseAlphabets: false,
+//   specialChars: false,
+//   lowerCaseAlphabets: false,
+// });
+
+// const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 minutes ahead
+
+// await Otp.create({
+//   email,
+//   otp,
+//   createdAt: Date.now(),
+//   expiresAt,
+// });
